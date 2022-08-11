d5b4b5c5f315c44a5b8c7ce72d4559d65577bb5e
ashish <ashishsachan919@gmail.com>
2022-08-11 16:59:08 +0530

    Better Logging

--- example/src/market_data_advanced_subscription/main.cpp
+++ example/src/market_data_advanced_subscription/main.cpp
@@ -4,9 +4,39 @@ Logger* Logger::logger = nullptr;  // This line is needed.
 class MyEventHandler : public EventHandler {
  public:
   bool processEvent(const Event& event, Session* session) override {
-    std::cout << toString(event) + "\n" << std::endl;
+  if(event.getType() != Event::Type::SUBSCRIPTION_DATA)
+    return true;
+    std::lock_guard<std::mutex> lock(m);
+    for (const auto& message : event.getMessageList()) {
+      auto correlationId = message.getCorrelationIdList().at(0);
+      time = message.getTime().time_since_epoch().count();
+      timeRecieved = message.getTimeReceived().time_since_epoch().count();
+      for (const auto& element : message.getElementList()) {
+        if(element.has("BID_PRICE")){
+          bestBidPrice = std::stod(element.getValue("BID_PRICE"));
+        }
+        if (element.has("ASK_PRICE")) {
+          bestAskPrice = std::stod(element.getValue("ASK_PRICE"));
+        }
+        if (element.has("BID_SIZE")) {
+          bestBidSize = std::stod(element.getValue("BID_SIZE"));
+        }
+        if (element.has("ASK_SIZE")) {
+          bestAskSize = std::stod(element.getValue("ASK_SIZE"));
+        }
+      }
+      std::cout<<timeRecieved<<","<<time<<","<<correlationId<<","<<bestBidPrice<<","<<bestAskPrice<<","<<bestBidSize<<","<<bestAskSize<<std::endl;
+    }
     return true;
   }
+ private:
+  mutable std::mutex m;
+  double bestBidPrice;
+  double bestAskPrice;
+  double bestBidSize;
+  double bestAskSize;
+  std::string symbol;
+  uint64_t time,timeRecieved;
 };
 class TradeEventHandler : public EventHandler {
  public:
